19927f4926a056852761011a284e6dfc11a9258a
Marlon Hänsdieke <marlon.haensdieke@check24.de>
2020-01-26 21:06:24 +0100

    Add rxjs tslint rules

--- tslint.js
+++ tslint.js
@@ -3,7 +3,8 @@ const path = require('path');
 module.exports = {
     extends: [
         'tslint-config-prettier',
-        'tslint-angular'
+        'tslint-angular',
+        'rxjs-tslint-rules'
     ],
     rulesDirectory: [
         path.join(path.dirname(require.resolve('codelyzer')), './'),
@@ -18,6 +19,23 @@ module.exports = {
                 'trailingComma': 'es5'
             }
         ],
-        'no-inferrable-types': false
+
+        // tslint-angular
+        'no-inferrable-types': false,
+
+        // rxjs-tslint-rules
+        'rxjs-finnish': {
+            'options': [{
+                'functions': false,
+                'methods': false,
+                'parameters': false,
+                'properties': true,
+                'variables': true
+            }]
+        },
+        'rxjs-no-exposed-subjects': true,
+        'rxjs-no-subject-unsubscribe': true,
+        'rxjs-no-unsafe-takeuntil': true,
+        'rxjs-prefer-angular-takeuntil': true
     }
 };
