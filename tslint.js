22a052206c23d5925440fdeda5717230fd8ed4b7
Marlon Hänsdieke <marlon.haensdieke@check24.de>
2020-02-26 10:53:33 +0100

    Update rxjs ruleset

--- tslint.js
+++ tslint.js
@@ -2,9 +2,9 @@ const path = require('path');
 
 module.exports = {
     extends: [
-        'tslint-config-prettier',
         'tslint-angular',
-        'rxjs-tslint-rules'
+        'rxjs-tslint-rules',
+        'tslint-config-prettier',
     ],
     rulesDirectory: [
         path.join(path.dirname(require.resolve('codelyzer')), './'),
@@ -20,30 +20,53 @@ module.exports = {
             }
         ],
 
-        // tslint-angular
+        // add/override tslint rules
+        'one-variable-per-declaration': [
+            true,
+            'ignore-for-loop'
+        ],
+        'max-classes-per-file': false,
+        'forin': false,
+
+        // override tslint-angular rules
         'no-inferrable-types': [
             true,
             'ignore-params',
             'ignore-properties'
         ],
-        'one-variable-per-declaration': [
-            true,
-            'ignore-for-loop'
-        ],
 
-        // rxjs-tslint-rules
+        // enable rxjs-tslint-rules
         'rxjs-finnish': {
-            'options': [{
-                'functions': false,
-                'methods': false,
-                'parameters': false,
-                'properties': true,
-                'variables': true
-            }]
+            options: [{
+                functions: false,
+                methods: false,
+                parameters: true,
+                properties: true,
+                variables: true
+            }],
+            severity: 'warning'
         },
-        'rxjs-no-exposed-subjects': true,
-        'rxjs-no-subject-unsubscribe': true,
+        'rxjs-no-exposed-subjects': { severity: 'warning' },
+        'rxjs-no-subject-unsubscribe': { severity: 'warning' },
         'rxjs-no-unsafe-takeuntil': true,
-        'rxjs-prefer-angular-takeuntil': true
+        'rxjs-prefer-angular-takeuntil': { severity: 'warning' },
+        'rxjs-no-compat': true,
+        'rxjs-no-create': true,
+        'rxjs-no-index': true,
+        'rxjs-no-internal': true,
+        'rxjs-no-nested-subscribe': true,
+        'rxjs-no-operator': true,
+        'rxjs-no-patched': true,
+        'rxjs-no-subclass': true,
+        'rxjs-no-unbound-methods': true,
+        'rxjs-no-add': true,
+        'rxjs-no-async-subscribe': true,
+        'rxjs-no-deep-operators': true,
+        'rxjs-no-ignored-error': { severity: 'warning' },
+        'rxjs-no-ignored-observable': true,
+        'rxjs-no-ignored-replay-buffer': true,
+        'rxjs-prefer-angular-async-pipe': { severity: 'warning' }
     }
 };
+
+
